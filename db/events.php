9685b037fb60ef59e6b3dcae0c19e86f3a3af4aa
Dan Marsden <dan@danmarsden.com>
2014-04-08 13:57:43 +1200

    Initial structure for new Moorsp plugin

---
+++ db/events.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Event handlers for the moorsp plagiarism plugin.
+ *
+ * @package    plagiarism_moorsp
+ * @copyright  2014 onwards Dan Marsden {@link http://danmarsden.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$handlers = array (
+
+    /*
+     * Event Handlers
+     */
+    'assessable_file_uploaded' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_file_uploaded',
+        'schedule'         => 'cron'
+    ),
+    'assessable_files_done' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_files_done',
+        'schedule'         => 'cron'
+    ),
+    'assessable_content_uploaded' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_content_uploaded',
+        'schedule'         => 'cron'
+    ),
+    'assessable_content_done' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_content_done',
+        'schedule'         => 'cron'
+    ),
+    'mod_created' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_mod_created',
+        'schedule'         => 'cron'
+    ),
+    'mod_updated' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_mod_updated',
+        'schedule'         => 'cron'
+    ),
+    'mod_deleted' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_mod_deleted',
+        'schedule'         => 'cron'
+    ),
+    'assessable_submitted' => array (
+        'handlerfile'      => '/plagiarism/moorsp/lib.php',
+        'handlerfunction'  => 'moorsp_event_assessable_submitted',
+        'schedule'         => 'cron'
+    ),
+
+);
---
+++ plagiarism_form.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * forms used by the moorsp plagiarism plugin.
+ *
+ * @package    plagiarism_moorsp
+ * @copyright  2014 onwards Dan Marsden {@link http://danmarsden.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once($CFG->dirroot.'/lib/formslib.php');
+
+class plagiarism_setup_form extends moodleform {
+
+/// Define the form
+    function definition () {
+        global $CFG;
+
+        $mform =& $this->_form;
+        $choices = array('No','Yes');
+        $mform->addElement('html', get_string('moorspexplain', 'plagiarism_moorsp'));
+        $mform->addElement('checkbox', 'moorsp_use', get_string('usemoorsp', 'plagiarism_moorsp'));
+
+        $mform->addElement('textarea', 'moorsp_student_disclosure', get_string('studentdisclosure','plagiarism_moorsp'),'wrap="virtual" rows="6" cols="50"');
+        $mform->addHelpButton('moorsp_student_disclosure', 'studentdisclosure', 'plagiarism_moorsp');
+        $mform->setDefault('moorsp_student_disclosure', get_string('studentdisclosuredefault','plagiarism_moorsp'));
+
+        $this->add_action_buttons(true);
+    }
+}
+
